cff69fe697a89bba243c20a023f022f9a06c189c
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-08 19:04:47 -0400

    basic config with hyprland

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+
+  description = "My NixOS config flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in 
+    {
+
+    nixosConfigurations = {
+      nixos-thinkpad = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+
+    homeConfigurations = {
+      julien = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+
+  };
+
+}
---
+++ git.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+  programs.git = {
+    enable = true;
+    userName = "Julien ouellet";
+    userEmail = "julienouellet@iusevimbtw.com";
+    extraConfig = {
+      pull.rebase = false;
+      init.defaultBranch = "main";
+    };
+  };
+}
