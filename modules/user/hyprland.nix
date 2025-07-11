68b461b1cd496ecd7ee68fe8eb9b0e5aadf586d5
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-10 21:46:39 -0400

    add workspaces keybinds

--- modules/user/hyprland.nix
+++ modules/user/hyprland.nix
@@ -10,7 +10,17 @@
         "$mod, RETURN, exec, kitty"
         "$mod, W, exec, firefox"
         "$mod, Q, killactive"
-      ];
+      ]
+      ++ (
+        builtins.concatLists (builtins.genList (i:
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
+       );
     };
   };
 }
