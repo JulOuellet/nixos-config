a7a1a38c257d376861403e785f003c7a3ba0c3c5
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-13 22:38:03 -0400

    less padding and more keybinds

--- modules/user/hyprland.nix
+++ modules/user/hyprland.nix
@@ -6,12 +6,17 @@
 
     settings = {
       "$mod" = "SUPER";
+      
       bind = [
-        # General
         "$mod, RETURN, exec, kitty"
         "$mod, W, exec, firefox"
-        "$mod, Q, killactive"
         "$mod, D, exec, fuzzel"
+
+        "$mod, Q, killactive"
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
 
         # Brightness
         ", XF86MonBrightnessUp, exec, brightnessctl set +10%"
@@ -21,17 +26,21 @@
         ", XF86AudioRaiseVolume, exec, pamixer -i 5"
         ", XF86AudioLowerVolume, exec, pamixer -d 5"
         ", XF86AudioMute, exec, pamixer -t"
-      ]
-      ++ (
+      ] ++ (
         builtins.concatLists (builtins.genList (i:
-            let ws = i + 1;
-            in [
-              "$mod, code:1${toString i}, workspace, ${toString ws}"
-              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-            ]
-          )
-          9)
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        ) 9)
       );
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+      };
+
       exec-once = "waybar";
     };
   };
