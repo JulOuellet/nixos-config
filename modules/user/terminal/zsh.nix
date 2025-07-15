9cb631c8120a6bb1459517ce29cbf312fc86ea31
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-15 17:46:06 -0400

    add p10k and colors in terminal

--- modules/user/terminal/zsh.nix
+++ modules/user/terminal/zsh.nix
@@ -3,21 +3,35 @@
 {
   programs.zsh = {
     enable = true;
-    
+
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    
+    oh-my-zsh = {
+      enable = true;
+      
+      plugins = [
+        "history-substring-search"
+      ];
+    };
 
     shellAliases = {
       ll = "ls -l";
       la = "ls -l -a";
     };
 
-    initContent = ''
-      source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-
-      bindkey '^[[A' history-substring-search-up
-      bindkey '^[[B' history-substring-search-down
-    '';
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = "p10k.zsh";
+      }
+    ];
   };
 }
