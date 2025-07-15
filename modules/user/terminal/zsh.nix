23bcae958774c56362e47839a9d76b06ea59d5aa
Julien ouellet <julienouellet@iusevimbtw.com>
2025-07-15 15:09:58 -0400

    add substring search zsh

--- modules/user/terminal/zsh.nix
+++ modules/user/terminal/zsh.nix
@@ -7,10 +7,17 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-    
+
     shellAliases = {
       ll = "ls -l";
       la = "ls -l -a";
     };
+
+    initContent = ''
+      source ${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+      bindkey '^[[A' history-substring-search-up
+      bindkey '^[[B' history-substring-search-down
+    '';
   };
 }
