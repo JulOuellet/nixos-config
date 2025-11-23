2cd489ccb473c7e37bc56d6e125408b4e983092b
Julien ouellet <julienouellet@iusevimbtw.com>
2025-11-23 12:10:08 -0500

    fastfetch rice

--- modules/user/terminal/fastfetch.nix
+++ modules/user/terminal/fastfetch.nix
@@ -9,6 +9,8 @@
     settings = {
       logo = {
         source = ''
+
+
               _   ___       ____  _____
              / | / (_)_  __/ __ \/ ___/
             /  |/ / /| |/_/ / / /\__ \
@@ -16,23 +18,65 @@
           /_/ |_/_//_/|_|\____//____/
         '';
         type = "data";
+        padding = {
+          left = 2;
+        };
       };
 
+      display = {
+        separator = " → ";
+      };
       modules = [
-        "title"
-        "separator"
-        "os"
-        "host"
-        "kernel"
-        "shell"
-        "wm"
-        "font"
-        "terminal"
-        "cpu"
-        "gpu"
-        "memory"
-        "disk"
-        "localip"
+        {
+          type = "host";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "os";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "kernel";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "wm";
+          key = " 󱂬";
+          keyColor = "blue";
+        }
+        {
+          type = "terminal";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "shell";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "cpu";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "gpu";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "memory";
+          key = " ";
+          keyColor = "blue";
+        }
+        {
+          type = "disk";
+          key = " ";
+          keyColor = "blue";
+        }
         "break"
       ];
     };
