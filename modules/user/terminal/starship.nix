2fd3774dca21e6b926bc3c0f4d4c7fe2a060d266
Julien ouellet <julienouellet@iusevimbtw.com>
2025-12-29 12:13:08 -0500

    update starship theme

--- modules/user/terminal/starship.nix
+++ modules/user/terminal/starship.nix
@@ -5,6 +5,43 @@
 }: {
   programs.starship = {
     enable = true;
-    settings = {};
+    settings = {
+      format = "$directory$git_branch$git_status$character";
+
+      add_newline = true;
+
+      directory = {
+        format = "[$path]($style) ";
+        style = "bold cyan";
+        truncation_length = 3;
+        truncate_to_repo = true;
+      };
+
+      git_branch = {
+        format = "[$symbol$branch]($style) ";
+        style = "bold purple";
+        symbol = " ";
+      };
+
+      git_status = {
+        format = "([\\[$all_status$ahead_behind\\]]($style) )";
+        style = "bold red";
+        conflicted = "=";
+        ahead = "⇡\${count}";
+        behind = "⇣\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        untracked = "?\${count}";
+        stashed = "$";
+        modified = "!\${count}";
+        staged = "+\${count}";
+        renamed = "»\${count}";
+        deleted = "✘\${count}";
+      };
+
+      character = {
+        success_symbol = "[➜](bold green)";
+        error_symbol = "[➜](bold red)";
+      };
+    };
   };
 }
